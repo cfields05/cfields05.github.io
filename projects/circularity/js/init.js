ffe352d9b251e834de1cbb7474a1f2c68b50b47b
Christian Fields <c.fields@ardencahillacademy.com>
2024-02-27 17:13:12 +0000

    refractor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -21,12 +21,23 @@ var init = function (window) {
         
         // TODO 1 : Declare and initialize our variables
 
+        var circle;
+        var circles = [];
 
         // TODO 2 : Create a function that draws a circle 
         
+        function drawCircle() {
+            circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+            physikz.addRandomVelocity(circle, canvas);
+            view.addChild(circle);
+            circles.push(circle);
+        }
 
         // TODO 3 / 7 : Call the drawCircle() function 
 
+        for (var i = 0; i < 100; i++) {
+            drawCircle()
+        }
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -40,9 +51,14 @@ var init = function (window) {
         function update() {
             // TODO 4 : Update the circle's position //
 
+            for (var i = 0; i < circles.length; i++) {
+                physikz.updatePosition(circles[i]);
+                game.checkCirclePosition(circles[i]);
+            }
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
            
+            // game.checkCirclePosition and physikz.updatePosition calls removed for loop efficiency
 
             // TODO 9 : Iterate over the array
            
@@ -56,14 +72,25 @@ var init = function (window) {
         */
         game.checkCirclePosition = function(circle) {
 
+            var rightEdge = circle.x + circle.radius;
+            var leftEdge = circle.x - circle.radius;
+            var topEdge = circle.y - circle.radius;
+            var bottomEdge = circle.y + circle.radius;
+
             // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
-            if ( circle.x > canvas.width ) {
-                circle.x = 0;
+            if ( leftEdge > canvas.width ) {
+                circle.x = 0 - circle.radius;
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
-
+        
+            if (rightEdge < 0) {
+                circle.x = canvas.width + circle.radius;
+            } if (topEdge > canvas.height) {
+                circle.y = 0 - circle.radius;
+            } if (bottomEdge < 0) {
+                circle.y = canvas.height + circle.radius;
+            }
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
