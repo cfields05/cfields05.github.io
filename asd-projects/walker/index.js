66a07c447d63759420eaa9d6b45d089a3c995dde
Christian Fields <c.fields@ardencahillacademy.com>
2024-06-11 21:16:50 +0000

    added walker functionality for keys being pressed or released as well as moving and redrawing walker

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -1,22 +1,38 @@
 /* global $, sessionStorage */
 
 $(document).ready(runProgram); // wait for the HTML / CSS elements of the page to fully load, then execute runProgram()
-  
-function runProgram(){
+
+function runProgram() {
   ////////////////////////////////////////////////////////////////////////////////
   //////////////////////////// SETUP /////////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
 
   // Constant Variables
-  var FRAME_RATE = 60;
-  var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
-  
+  const FRAME_RATE = 60;
+  const FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
+
   // Game Item Objects
 
+  // KEY object holds key codes that the program will respond to when pressed
+  const KEY = {
+    LEFT: 37,
+    RIGHT: 39,
+    UP: 38,
+    DOWN: 40,
+  }
+
+  // WALKER object holds data for the walker (x-position, y-position, speed along x-axis, speed along y-axis)
+  const WALKER = {
+    x: 0,
+    y: 0,
+    speedX: 0,
+    speedY: 0,
+  }
 
   // one-time setup
-  var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
+  const interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
+  $(document).on('keydown', handleKeyDown);                             // event handler to listen for when a key is pressed and run handleKeyDown()
+  $(document).on('keyup', handleKeyUp);                                 // event handler to listen for when a key is released and run handleKeyUp()
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -27,22 +43,55 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
+    repositionGameItem(WALKER);
 
   }
-  
-  /* 
-  Called in response to events.
-  */
-  function handleEvent(event) {
 
+  // Handles keys being pressed at any time and adjusts speed if key pressed is left, right, up, or down
+  function handleKeyDown(event) {
+    switch (event.which) {
+      case KEY.LEFT:
+        WALKER.speedX = -5;
+        break;
+      case KEY.RIGHT:
+        WALKER.speedX = 5;
+        break;
+      case KEY.UP:
+        WALKER.speedY = -5;
+        break;
+      case KEY.DOWN:
+        WALKER.speedY = 5;
+        break;
+    }
+  }
+
+  // Handles keys being released and resets respective speed values to 0 when directional key is let go
+  function handleKeyUp(event) {
+    switch (event.which) {
+      case KEY.LEFT:
+      case KEY.RIGHT:
+        WALKER.speedX = 0;
+        break;
+      case KEY.UP:
+      case KEY.DOWN:
+        WALKER.speedY = 0;
+        break;
+    }
   }
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
+  function repositionGameItem(walker) {
+    walker.x += walker.speedX;
+    walker.y += walker.speedY;
+    redrawGameItem(walker);
+  }
 
-  
+  function redrawGameItem(walker) {
+    $('#walker').css({ 'left': walker.x, 'top': walker.y });
+  }
+
   function endGame() {
     // stop the interval timer
     clearInterval(interval);
@@ -50,5 +99,5 @@ function runProgram(){
     // turn off event handlers
     $(document).off();
   }
-  
+
 }
